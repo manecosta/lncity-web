462133e239f5af617129761a9bd8cb70ed6b1c8b
Manuel Costa <manuel.costa@kelvininc.com>
2019-03-26 01:49:36 +0000

    editor configurations and adding back the coming soon landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ComingSoonComponent } from './components/coming-soon/coming-soon.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, ComingSoonComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent, ComingSoonComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-coming-soon',
+    templateUrl: 'coming-soon.component.html',
+    styleUrls: ['coming-soon.component.less']
+})
+export class ComingSoonComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
